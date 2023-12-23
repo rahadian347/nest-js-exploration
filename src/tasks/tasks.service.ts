76b7991b41729ca4f60159375d053aed09fed608
Rahadian Permana <rahadian.permana@moladin.com>
2023-12-23 13:19:00 +0700

    ad CRUD task-management and implement DTO

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find((a) => a.id === id);
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const task: Task = {
+      id: uuid(),
+      status: TaskStatus.OPEN,
+      ...createTaskDto,
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+}
